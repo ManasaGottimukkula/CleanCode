bfe85e45d0de0258e3480c3aab1ccf44e8e6c484
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-02 19:14:46 +0530

    updated classes

--- InterestCalculator/src/main/java/com/epam/InterestCalculator/IntrestCal.java
+++ InterestCalculator/src/main/java/com/epam/InterestCalculator/IntrestCal.java
@@ -3,7 +3,8 @@ package com.epam.InterestCalculator;
 public class IntrestCal {
 
 	public static void main(String[] args) {
-		
+		Construction.CostEstimation();
+		SimpleInterest.simpleInterestCalculator();
 	}
 
 }
--- InterestCalculator/src/main/java/com/epam/InterestCalculator/SimpleInterest.java
+++ InterestCalculator/src/main/java/com/epam/InterestCalculator/SimpleInterest.java
@@ -1,5 +1,31 @@
 package com.epam.InterestCalculator;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class SimpleInterest {
+	
+	
+	public static void simpleInterestCalculator() {
+	Scanner sc = new Scanner(System.in);
+	PrintStream ps = new PrintStream(new FileOutputStream(FileDescriptor.out));
+	ps.print("Please enter principle amount :");
+	try {
+		double prAmount = sc.nextDouble();
+		ps.println("Enter the time period");
+		double timePeriod = sc.nextDouble();
+		ps.println("Enter the rate of interest per year");
+		double rateInterest = sc.nextDouble();
+		double simpInt = (prAmount * timePeriod * rateInterest) / 100;
+		double cmpInt = (prAmount * Math.pow(1 + (rateInterest/100), timePeriod)) - prAmount;
+		ps.println("Simple Interest is :" + simpInt);
+		ps.println("Compound Interest is :"+ cmpInt);
+	}
+	catch(Exception e) {
+		ps.println("Invalid input!\nEnter the valid one!");
+	}
+}
 
 }

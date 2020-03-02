07e31467c8e1a1f94793b0993d3ac5f1e0f2abf7
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-02 19:01:25 +0530

    modified Construction class

--- InterestCalculator/src/main/java/com/epam/InterestCalculator/Construction.java
+++ InterestCalculator/src/main/java/com/epam/InterestCalculator/Construction.java
@@ -1,5 +1,47 @@
 package com.epam.InterestCalculator;
 
-public class Construction {
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
+public class Construction {
+	public static void CostEstimation() {
+		
+		Scanner sc = new Scanner(System.in);
+		PrintStream ps = new PrintStream(new FileOutputStream(FileDescriptor.out));
+		ps.println("Choose one from the available materials..\n1. Construction with Standard materials\n2. Construction with above standard materials\n3. Construction with High standard materials\n4. Construction with High standard materials and fully automated home");
+		try {
+			int choose = sc.nextInt();
+			double houseArea;
+			switch(choose) {
+			case 1:
+				ps.println("Please enter the area of house in sqfts :");
+				houseArea = sc.nextDouble();
+				ps.println("Total estimated cost is " + (1200 * houseArea));
+				break;
+			case 2:
+				ps.println("Please enter the area of house in sqfts :");
+				houseArea = sc.nextDouble();
+				ps.println("Total estimated cost is " + (1500 * houseArea));
+				break;
+			case 3:
+				ps.println("Please enter the area of house in sqfts :");
+				houseArea = sc.nextDouble();
+				ps.println("Total estimated cost is " + (1800 * houseArea));
+				break;
+			case 4:
+				ps.println("Please enter the area of house in sqfts :");
+				houseArea = sc.nextDouble();
+				ps.println("Total estimated cost is " + (2500 * houseArea));
+				break;
+			default:
+				ps.println("Select from the above options!");
+			}
+		}
+			catch(Exception e) {
+				ps.println("Invalid input!!");
+		}
+		
+	}
 }

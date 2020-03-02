8c156d0894e9ceb2fb1b6dfa39573314dae94440
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-02 17:55:20 +0530

    created project for interestCalculator

---
+++ InterestCalculator/src/main/java/com/epam/InterestCalculator/App.java
@@ -0,0 +1,13 @@
+package com.epam.InterestCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
